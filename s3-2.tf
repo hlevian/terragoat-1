a7a16d5c0c1f50f904788ba363470b4b12cc2ea2
hlevian <135955824+hlevian@users.noreply.github.com>
2023-08-02 10:42:42 +0300

    Update s3-2.tf
    Co-authored-by: bridgecrew-dev[bot] <83754225+bridgecrew-dev[bot]@users.noreply.github.com>

--- s3-2.tf
+++ s3-2.tf
@@ -117,7 +117,6 @@ resource "aws_s3_bucket" "logs" {
     enabled = true
   }
   server_side_encryption_configuration {
-    rule {
       apply_server_side_encryption_by_default {
         sse_algorithm     = "aws:kms"
         kms_master_key_id = "${aws_kms_key.logs_key.arn}"
